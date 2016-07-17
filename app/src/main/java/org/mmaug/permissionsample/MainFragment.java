79f10f9fbe332c5785401a5c2e0af5943960452d
Ye Lin Aung <me@yelinaung.com>
2016-07-17 23:44:04 +0800

    remove wrong import left over from refactoring the package

--- app/src/main/java/org/mmaug/permissionsample/MainFragment.java
+++ app/src/main/java/org/mmaug/permissionsample/MainFragment.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
-import com.yelinaung.permissionsample.R;
 import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.EasyPermissions;
 
